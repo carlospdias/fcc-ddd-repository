2edb30eae2b1ba1147bcc87052db12f8d446f0c8
Carlos Pereira Dias <carlospdiase@gmail.com>
2026-07-12 21:04:51 -0300

    Cria order respository

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,8 +1,22 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderRepositoryInterface from "../../../../domain/checkout/repository/order-repository.interface";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface{
+
+
+
+  update(entity: Order): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  find(id: string): Promise<Order> {
+    throw new Error("Method not implemented.");
+  }
+  findAll(): Promise<Order[]> {
+    throw new Error("Method not implemented.");
+  }
+  
   async create(entity: Order): Promise<void> {
     await OrderModel.create(
       {
